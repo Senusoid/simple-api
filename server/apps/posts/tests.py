105466069089155fc4c77d1042ca3744f1733fc0
abuilov <albuilov@gmail.com>
2019-05-13 15:38:30 +0400

    add posts, votes tests

--- server/apps/posts/tests.py
+++ server/apps/posts/tests.py
@@ -8,22 +8,55 @@ from rest_framework.views import status
 from .models import Post, Vote
 
 
-class BaseViewTest(APITestCase):
-    client = APIClient()
+class PostsTests(APITestCase):
+    user_data = {
+        'username': 'qwe',
+        'password': 'asd',
+        'email': 'qwe@qwe.com'
+    }
+
+    post_data = {
+        'title': 'breakin news',
+        'text': 'abrakadabraololol'
+    }
+
+    def get_token(self, user_data):
+        url_path = reverse('users:login')
+
+        response = self.client.post(url_path, data=user_data, format='json')
+        return response.data.get('token')
 
     def setUp(self):
-        self.user = User.objects.create(username='qwe', password='asd', email='qwe@qwe.com')
-        self.post_data = {
-            'title': 'breakin news',
-            'text': 'abrakadabraololol',
-            'user': self.user.id
-        }
-
-
-class PostCreationTest(BaseViewTest):
+        self.user = User.objects.create_user(**self.user_data)
 
     def test_creation_post(self):
-        response = self.client.post('posts', data=self.post_data)
+        url_path = reverse('posts:posts-list')
+        token = self.get_token(self.user_data)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        response = self.client.post(url_path, data=self.post_data, format='json')
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        post_id = response.data.get('id')
+        post = Post.objects.get(id=post_id)
+        self.assertEqual(post.user, self.user)
 
+    def test_post_vote(self):
+        like = {'value': 1}
+        dislike = {'value': -1}
+
+        post = self.user.post_set.create(**self.post_data)
+        url_path = reverse('posts:posts-vote', kwargs={"pk": post.id})
+
+        token = self.get_token(self.user_data)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        response = self.client.post(url_path, data=like, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.post(url_path, data=dislike, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.delete(url_path, data=like, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- server/apps/posts/urls.py
+++ server/apps/posts/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 
+app_name = 'posts'
+
 router = DefaultRouter()
-router.register('', views.PostsViewSet)
+router.register('', views.PostsViewSet, base_name='posts')
 
 urlpatterns = [
     path('', include(router.urls))
